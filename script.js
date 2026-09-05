4af3433d6999acc63b5f5c93312cc587fc8f2a39
anshad-digital <anshaad.s@gmail.com>
2026-09-05 12:53:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+const btn=document.getElementById('menuBtn');const nav=document.getElementById('mobileNav');btn.addEventListener('click',()=>nav.classList.toggle('hidden'));document.querySelectorAll('#mobileNav a').forEach(a=>a.addEventListener('click',()=>nav.classList.add('hidden')));
